786d0058879cec22451c09bb5ea1db1e36158799
gadarajainik <gadarajainik1@gmail..com>
2024-03-21 15:54:56 -0500

    GEOC-10 Login page

--- GeoCompensate_app/App.js
+++ GeoCompensate_app/App.js
@@ -1,16 +1,16 @@
 import React, {useState} from 'react';
 import HomeScreen from './src/screens/HomeScreen';
-import {Login} from './src/screens/Login';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from './src/screens/Login';
 
 const Stack = createNativeStackNavigator();
 
 function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
 }
