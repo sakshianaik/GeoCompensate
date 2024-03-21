beab9093c0bec9a53973b62afd8d092c820b9298
Hetvi Joshi <hetvijoshi510@gmail.com>
2024-03-20 19:23:28 -0500

    GEOC-11 Login page

--- GeoCompensate_app/App.js
+++ GeoCompensate_app/App.js
@@ -3,24 +3,10 @@ import {SafeAreaView, useColorScheme} from 'react-native';
 import {Button} from 'react-native-paper';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Login from './src/screens/Login';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <Button
-        icon="camera"
-        mode="outlined"
-        onPress={() => console.log('Pressed')}>
-        Press me
-      </Button>
-    </SafeAreaView>
-  );
+  return <Login />;
 }
 
 export default App;
